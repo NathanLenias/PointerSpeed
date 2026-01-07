64ff43e6d5ed58750ace72b410fe492217b9c834
NathanLenias <nathan.balencie@gmail.com>
2026-01-07 20:03:42 +0100

    Initial Commit

---
+++ PointerSpeed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PointerSpeed
+//
+//  Created by Nathan Balencie on 07/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PointerSpeed/PointerSpeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointerSpeedApp.swift
+//  PointerSpeed
+//
+//  Created by Nathan Balencie on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct PointerSpeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
